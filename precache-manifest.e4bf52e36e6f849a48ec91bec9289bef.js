30cb423760df4c0e94dcbf3618494f72ad6ccf80
tamara-f <filatovatam@ukr.net>
2020-09-01 20:38:59 +0300

    Updates

---
+++ precache-manifest.e4bf52e36e6f849a48ec91bec9289bef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee12e8a747a455beeb63e988994c732",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "2c90d17a3b1fe88b9305",
+    "url": "/goit-react-hw-02-feedback/static/css/main.d12b5bc3.chunk.css"
+  },
+  {
+    "revision": "dfecf363b9111312fdaa",
+    "url": "/goit-react-hw-02-feedback/static/js/2.18cad9f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.18cad9f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c90d17a3b1fe88b9305",
+    "url": "/goit-react-hw-02-feedback/static/js/main.8533f2fc.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
